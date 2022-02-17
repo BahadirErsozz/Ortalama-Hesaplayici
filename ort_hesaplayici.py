c51b2907a790cc5151f4ea098d167e813fcafe95
BahadirErsozz <68385105+BahadirErsozz@users.noreply.github.com>
2022-02-17 14:05:13 +0300

    Add files via upload
    ex.txt dosyası eklendi, ort_hesaplayici.py dosyasına yorum satırları eklendi ve output için birkaç değişiklik yapıldı.

--- ort_hesaplayici.py
+++ ort_hesaplayici.py
@@ -1,5 +1,7 @@
-ogrenci_no = input("Öğrenci numaranızı giriniz:")
+ogrenci_no = input("Okunacak dosya ismini giriniz: ")
+# Harf notlarinin 4'luk sistemdeki karsiliklarini gostermektedir. Eger notlandirma sisteminiz farkli ise assagidan degistirebilirsiniz.
 gecerli_not_listesi = {"AA": 4, "BA": 3.5, "BB": 3, "CB": 2.5, "CC": 2, "DC": 1.5, "DD": 1, "DF": 0.5, "FF": 0}
+# Program sadece listedeki dersler icin calisiyor. Assagiya ders ek0leyebilirsiniz(kac kredi oldugunu assagiya yazmayi unutmayin).
 ders_listesi = ["BİL103", "BİL113", "MAT101", "FİZ101", "FİZ101L", "TÜR101", "İNG001", "BİL211", "BİL132", "MAT102", "FİZ102", "FİZ102L", "TÜR102", "İNG002", "BİL133", "BİL212", "BİL265", "BİL245", "AİT201", "İNG003", "BİL214", "BİL334", "BİL361", "İKT105", "OEG101", "AİT202", "İNG004", "BİL331", "BİL395", "BİL481", "BİL345", "UGİ315", "İYD1", "BİL372", "BİL372", "BİL461", "BilimSD", "BSD1", "İYD2", "BİL452", "BİL495", "FSD", "BSD2", "BSD3", "İYD3", "BİL496", "BSD4", "BSD5", "ÜSD", "İYD4"]
 olan_dersler = []
 notlar = {}
@@ -7,6 +9,7 @@ toplam_ders_kredisi = 0
 not_toplam = 0
 z = 1
 hata_yok = True
+# Ders ekledikten sonra kac kredi oldugunu da yazmak gerekmektedir.
 ders_kredileri = {"BİL103": 2, "BİL113": 4, "MAT101": 4, "FİZ101": 3, "FİZ101L": 1, "TÜR101": 2, "İNG001": 2, "BİL211": 4, "BİL132": 3, "MAT102": 4, "FİZ102": 3, "FİZ102L": 1, "TÜR102": 2, "İNG002": 2, "BİL133": 3, "BİL212": 4, "BİL265": 4, "BİL245": 4, "AİT201": 2, "İNG003": 2, "BİL214": 4, "BİL334": 3, "BİL361": 3, "İKT105": 3, "OEG101": 1, "AİT202": 2, "İNG004": 2, "BİL331": 3, "BİL395": 3, "BİL481": 3, "BİL345": 3, "UGİ315": 2, "İYD1": 3, "BİL372": 4, "BİL461": 3, "BilimSD": 3, "BSD1": 3, "İYD2": 3, "BİL452": 3, "BİL495": 1, "FSD": 3, "BSD2": 3, "BSD3": 3, "İYD3": 3, "BİL496": 4, "BSD4": 3, "BSD5": 3, "ÜSD": 3, "İYD4": 3}
 try:
     f = open(ogrenci_no + ".txt", "r", encoding="utf-8")
@@ -23,7 +26,7 @@ try:
 
 except:
     if z == 1:
-        print("Geçersiz öğrenci numarası!")
+        print("Geçersiz dosya ismi")
         hata_yok = False
         exit()
 
@@ -38,4 +41,4 @@ for x in notlar:
 
 if hata_yok:    # == True çıkardım
     ano = not_toplam / toplam_ders_kredisi
-    print(ogrenci_no + " no'lu öğrencinin not ortalaması: " + str(ano)[0:4])
+    print("Hesaplanan not ortalamanız: " + str(ano)[0:4] + "(4 üzerinden) " + str(ano * 25)[0:4] + "(100 üzerinden)")
